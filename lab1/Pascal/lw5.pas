f2a22a5ff54a0afaa27f09a3fbd036039e2a21c4
Sharapor <83550057+SharapovTact@users.noreply.github.com>
2026-02-13 00:06:36 +0300

    Исправил 5-е задание

--- lab1/Pascal/lw5.pas
+++ lab1/Pascal/lw5.pas
@@ -1,33 +1,16 @@
 PROGRAM SarahRevere(INPUT, OUTPUT);
 USES
   GPC;
-VAR
-  QueryString, Lanterns, Response: STRING;
-  PosLanterns: INTEGER;
 BEGIN
-  Response := 'The British are coming from ';
-  QueryString := GetEnv('QUERY_STRING');
-  PosLanterns := Pos('lanterns=', QueryString);
-  
-  IF PosLanterns > 0 
-  THEN
-    BEGIN
-      Delete(QueryString, 1, PosLanterns + 8);
-      Lanterns := QueryString
-  END;
-  
-  IF Lanterns = '1'
-  THEN
-    Response := Response + 'land!'
-  ELSE
-    IF Lanterns = '2'
-    THEN
-      Response := Response + 'sea!'
-    ELSE
-      Response := 'Sarah, what do you mean?';
-  
-  
   WRITELN('Content-type: text/html');
   WRITELN;
-  WRITELN(Response)
+  IF (GetEnv('QUERY_STRING') = 'lanterns=1')
+  THEN
+    WRITELN('The British are coming from land!')
+  ELSE
+    IF (GetEnv('QUERY_STRING') = 'lanterns=2')
+    THEN
+      WRITELN('The British are coming from sea!')
+    ELSE
+      WRITELN('Sarah, what do you mean?');
 END.
--- lab1/README.txt
+++ lab1/README.txt
@@ -1,1 +1,2 @@
 lw4: http://localhost/test-cgi/lw4.cgi?Nikita
+lw5: http://localhost/test-cgi/lw5.cgi?lanterns=1
