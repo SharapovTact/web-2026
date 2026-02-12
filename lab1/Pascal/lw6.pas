a395173056c3693296c33b663537faed1a6cf329
Sharapor <83550057+SharapovTact@users.noreply.github.com>
2026-02-13 00:31:48 +0300

    Сделал 6-е задание

---
+++ lab1/Pascal/lw6.pas
@@ -0,0 +1,21 @@
+PROGRAM Hello(INPUT, OUTPUT);
+USES
+  GPC;
+VAR
+  QueryString, Name, Response: STRING;
+BEGIN
+  QueryString := GetEnv('QUERY_STRING');
+  Response := 'Hello ';
+  IF Pos('name=', QueryString) > 0
+  THEN
+    BEGIN
+      Name := Copy(QueryString, 6, Length(QueryString) - 5);
+      Response := Response + 'dear, ' + Name + '!'
+    END
+  ELSE
+    Response := Response + 'Anonymous!';
+
+  WRITELN('Content-type: text/html');
+  WRITELN;
+  WRITELN(Response)
+END.
--- lab1/README.txt
+++ lab1/README.txt
@@ -1,2 +1,3 @@
 lw4: http://localhost/test-cgi/lw4.cgi?Nikita
 lw5: http://localhost/test-cgi/lw5.cgi?lanterns=1
+lw6: http://localhost/test-cgi/lw6.cgi?name=Nikita
