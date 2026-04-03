4dbc70bd8c5fa9228d599042c66916f3009cd0d1
Sharapor <83550057+SharapovTact@users.noreply.github.com>
2026-04-03 09:13:41 +0300

    Сделал отображение всех картинок в посте, обработал картинки как необяз. параметр

--- social-media/home/post_preview.php
+++ social-media/home/post_preview.php
@@ -7,25 +7,25 @@
         <img class="header__edit-image" src="../images/edit.png" alt="Edit" width="20" height="20">
     </div>
     <div class="post__content">
-        <div class="content__images">
-            <a href="<?= '../home/?postId=' . $post['postId'] ?>">
-                <img class="images__image" src="<?= '../images/' .  $post['images'][0] ?>" alt="Front image" width="474" height="474">
-            </a>
-        </div>
-        <img class="images__indicator" src="../images/indicator.png" alt="Indicator" width="44" height="24">
-        <img class="content__reaction" src="../images/reaction.png" alt="Reaction" width="68" height="30">
-        <?php
-        if (!empty($post['description'])) {
-            echo '<p class="content__description-short text">' . htmlspecialchars($post['description']) . '</p>';
-        }
-        ?>
+        <?php if (isset($post['images'])){
+            foreach ($post['images'] as $image){?>
+            <div class="content__images">
+                <a href="<?= '../home/?postId=' . $post['postId'] ?>">
+                    <img class="images__image" src="<?= '../images/' .  $image ?>" alt="Front image" width="474" height="474">
+                </a>
+            </div>
+            <?php } if (sizeof($post['images']) > 1){ ?>
+                <img class="images__indicator" src="../images/indicator.png" alt="Indicator" width="44" height="24">
+            <?php } ?>
+        <?php } ?>
 
-        <?php
-        if (!empty($post['description'])) {
-            echo '<a class="content__show-more text" title="Click to see more">ещё</a>';
-        }
-        ?>
-        <p class="content__time-ago text"><?php
+        <img class="content__reaction" src="../images/reaction.png" alt="Reaction" width="68" height="30">
+        <?php if (!empty($post['description'])) { ?>
+            <p class="content__description-short text"> <?= $post['description'] ?> </p>
+            <a class="content__show-more text" title="Click to see more">ещё</a>
+        <?php } ?>
+        <p class="content__time-ago text">
+            <?php
             echo timeAgo($post['createdAt']);
             ?></p>
     </div>
